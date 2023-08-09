2d48e95fe8f647fba9c8e84d5c2169e0b1f428de
James Eapen <james.eapen@vai.org>
2023-08-09 17:27:26 -0400

    fix(hm:core): add isync override with cyrus-sasl-xoauth2 plugin

--- config/core.nix
+++ config/core.nix
@@ -1,5 +1,18 @@
 { config, pkgs, dotfiles, ... }:
 
+let
+  # Inspiration: https://github.com/NixOS/nixpkgs/issues/108480#issuecomment-1115108802
+  isync-oauth2 = with pkgs; buildEnv {
+    name = "isync-oauth2";
+    paths = [ isync ];
+    pathsToLink = ["/bin"];
+    nativeBuildInputs = [ makeWrapper ];
+    postBuild = ''
+        wrapProgram "$out/bin/mbsync" \
+          --prefix SASL_PATH : "${cyrus_sasl}/lib/sasl2:${cyrus-sasl-xoauth2}/lib/sasl2"
+      '';
+  };
+in
 {
   home = {
     packages = with pkgs; [
@@ -15,7 +28,7 @@
       git
       gnumake
       htop
-      isync
+      isync-oauth2
       lynx
       msmtp
       neomutt
