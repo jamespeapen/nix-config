196dbcfa55961338078564adb6187fcfe84e8e1d
James Eapen <james.eapen@vai.org>
2025-02-18 16:04:52 -0500

    new(work): add dplyr and httpgd

--- config/work.nix
+++ config/work.nix
@@ -19,7 +19,7 @@ tinyplot = rPackages.buildRPackage {
     sha256 = "XsxhMWok1pT29XS5q6KQ3l6l+v7p6wAg4703uKAcCEU=";
   };
 };
-base-R = rWrapper.override { packages = with rPackages; [ colorout data_table tinyplot knitr languageserver quarto pak rmarkdown ]; };
+base-R = rWrapper.override { packages = with rPackages; [ collapse colorout data_table dplyr tinyplot httpgd knitr languageserver quarto pak rmarkdown ]; };
 in
 {
   home = {
